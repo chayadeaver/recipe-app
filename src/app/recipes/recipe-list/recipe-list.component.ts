6a66288ce9032a360e5429a157025117f56153ea
chayadeaver <chayadeaver@gmail.com>
2021-01-16 16:08:35 -0500

    dyanamically add data to populate list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d')
+  ];
   constructor() { }
 
   ngOnInit(): void {
