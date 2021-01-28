c19fd7c8d010569ee2f087c64adca685a3e25d2e
chayadeaver <chayadeaver@gmail.com>
2021-01-28 13:17:33 -0500

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,13 +6,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d')
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
