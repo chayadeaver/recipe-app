a7b05706a9c0a8a13fbca93098373caa18fccc50
chayadeaver <chayadeaver@gmail.com>
2021-02-15 14:44:09 -0500

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,30 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/e9/46/avocado_bread_breakfast_cooking_cuisine_delicious_diet_dinner-1515351.jpg!d')
+    new Recipe(
+      'Biscuit Taco Casserole',
+      'Stretch a half-pound of ground beef deliciously into eight hearty servings!',
+      'https://images-gmi-pmc.edge-generalmills.com/c24bc565-4f51-4064-9beb-4fd10aa6d8e5.jpg',
+      [
+        new Ingredient('Ground beef', 1),
+        new Ingredient('Biscuits', 1),
+        new Ingredient('Taco sauce', 1)
+
+      ]),
+    new Recipe(
+      'Spaghetti and Meatballs',
+      "Making your sauce is always better—it's cheaper and so much more flavorful. This one comes together in about 15 minutes and is the perfect accompaniment to the big garlicky meatballs.",
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/1600x2399/gallery-1506456122-delish-spaghetti-meatballs-2.jpg?resize=768:*',
+      [
+        new Ingredient('Spaghetti pasta', 16),
+        new Ingredient('Ground beef', 16),
+        new Ingredient('Canned Tomatoes', 3)
+      ])
   ];
 
   getRecipes() {
